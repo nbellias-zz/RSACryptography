b47b2950be89345e73bae6f03d1d3ea1cbb3db28
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2022-04-15 09:04:43 +0300

    Some corrections

--- Program.cs
+++ Program.cs
@@ -24,12 +24,39 @@ Console.WriteLine();
 //Console.WriteLine();
 //Console.WriteLine(plainText);
 
-RSAEncryptionWithOwnX509Cert rsaX509 = new RSAEncryptionWithOwnX509Cert();
+//**************************************************************************
+string sessionID = Guid.NewGuid().ToString("N").ToUpper();
+string version = "4.0";
+string cardNumber = "4016360000000010";
+string expirationDate = "2206";
+string deviceCategory = "0";
+string cardAmount = "5";
+string exponent = "2";
+string description = "Test http post MPI request";
+string currency = "978";
+string MID = "0022007645";
+string xid = "aTb+aVdPcawghwB9k4J1B5/hAG8=";
+string merchantTxId = Guid.NewGuid().ToString("D"); //"221f3245-88d8-4a1f-ab7e-5348567999e5"; // 
+string okUrl = $"https://alphaecommerce-test.cardlink.gr:443/coffeehouse/MerchantHandler2;jsessionid={sessionID}?resultUrl=OK";
+string failUrl = $"https://alphaecommerce-test.cardlink.gr:443/coffeehouse/MerchantHandler2;jsessionid={sessionID}?resultUrl=FAIL";
+string MD = sessionID;
+string city = "City";
+string streetNum = "300";
+string street = "Street";
+string zipCode = "12345";
+string valueToSignForSignature = $"{version};{cardNumber};{expirationDate};{deviceCategory};{cardAmount};{exponent};{description};{currency};{MID};{xid};{merchantTxId};{okUrl};{failUrl};{MD};{city};{streetNum};{street};{zipCode};";
 
-Console.WriteLine(RSAEncryptionWithOwnX509Cert.sign("Hello World"));
+RSAEncryptionWithOwnX509Cert rsaX509 = new RSAEncryptionWithOwnX509Cert();
+Console.WriteLine($"SESSIONID = {sessionID}");
 Console.WriteLine();
+Console.WriteLine($"merchantTxId = {merchantTxId}");
 Console.WriteLine();
-Console.WriteLine(RSAEncryptionWithOwnX509Cert.sign2("Hello World"));
+Console.WriteLine("Signature below:");
 Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine(RSAEncryptionWithOwnX509Cert.sign3("Hello World"));
+//Console.WriteLine(RSAEncryptionWithOwnX509Cert.sign(valueToSignForSignature));
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine(RSAEncryptionWithOwnX509Cert.sign2(valueToSignForSignature));
+//Console.WriteLine();
+//Console.WriteLine();
+Console.WriteLine(RSAEncryptionWithOwnX509Cert.sign3(valueToSignForSignature));
--- RSAEncryptionWithOwnX509Cert.cs
+++ RSAEncryptionWithOwnX509Cert.cs
@@ -12,67 +12,79 @@ namespace RSACryptography
     internal class RSAEncryptionWithOwnX509Cert
     {
         // I used "openssl req -sha256 -newkey rsa:2048 -nodes -keyout merchant.pem -x509 -days 1440 -out merchant_x509o.cer" on my Mac to produce the following keys
-
-        private static String myX509CertificatePEMinXML = @"<RSAKeyValue><Modulus>rhZ0QUvVZol3UJ3a86PNAnMwWNOvkEhQdda7HIeGFhIkEiM7m/suJcSQdaj8jpI4yB6UbPogUgYzdFlZxYDmeBZKdudJocO68wTJPiAJrITyHDAwcD/Kz9huLrLPsX+1rLPc1dWPxxUbpGrc4hPpLlbwf6FYJHP14o518NkgJy+v0rSeAa6Ak41qwLyAyK0Jzi/wcrHe40tBLQe6k40QftbsTxkgEkUQ7+G718f7DpQ9zb3xOfuk3J9NtCCliCk+APUOAtduOOR8rxeTOhc4eVSrXUou+tthKOTBrOqrMibplFIZKAwvgQ8TfasSRlVzZf+kY/tfxvcmlV/j98D1XQ==</Modulus><Exponent>AQAB</Exponent><P>4oUrDnw11g/a7F6Lmo9MuSpyJv+9PeDHeIlFsvya9iofDvtrrWoFVtQYnhs3v/kqwERI/AWst5M9xj1zHWXeBY3ClSrbEzFbN0iPA78EMVbAwR92LwCkjOLf+YJdAWmZGzPLNBMI30eR5PCoYSqfz8hquSDtygrPBYtshoIjhjM=</P><Q>xL5tAcBF0tcbhzPawvh18DzLiyCjX9Y14U4zheN4PSbOgfcd7Qtx+MiP7GuFku4UZsdxWDRDzrCfjtIlYfFpfcpGaNjERV7GEo4cg3Bj7vwkV16JkPWYJV20Vvfo3Tfv1C8IUmPQE2fpJL3ch3Kw17ryxCSbM+JMA45H89QxZi8=</Q><DP>id8F6uYdGnJCDJYrIGLSDu3P1v7XXx8+KZZbrupjIytWBhdvXXhnUYb8iNcGIrl0lNu7FHIbwwxCkDN8qksDTJwQ8BvKha8H1uk56szLr4AfCCyvEUKx5PJ94m6/TwnH36+xobtoLgZwvUjhI2r/JEfW9WEeelgD01seLext1NM=</DP><DQ>ssF110Zz6loJJ/GkTMtklM8X/KO97MB7GedshrSjNaSBsrHtHcftWKYZpiwXI+Vu4edKA2MRTMVLY39p+bBXMbRL/s0cnw66FnfKTl4aXjP3bO53PR73itO1m+MJT52YKQhGnl0T293ohbp/mQ1KweeY1T+RSDXO7NgkZdSgwX8=</DQ><InverseQ>ujqvtOHAzBSJuJ+nn9O/JJUlP7FfR9U1PzxHBh2x8AvmZHQ16n/E8ZExgGmnQUi4rdAL9ENurKA2DCYW7cXfIPxdLI+jUulFEUnD6I18yip7TW3vIKnDtL10YD0AUop5tVWeiTQVbyXGkpuOlgTHa4QbDzUaBOF/7JPq9jHavQY=</InverseQ><D>A9lZPqxXjonRioXC9ooTmU9ErWIYyp7esIxL3MieTVSfWYtP4RX/ojQjYYaX9usCuGH8tCAfoNCkSJlFI9nCvrr3cIOSnDIAqyja7jW1IQxbcgYavBCdAlVtsjif4PPK54OeiVfMcwFjeBIoyhLYn/6qGJhPo5awxA6USvvx+wvIgqmF9Os324gG9Q5QfznvYbdPa3CCeWFLkReyhG5YVpCHRZeiw6HE+XsVPQ54Q05dT3LXAM4+IfB/99tOxNvozZ1SiTkLOYz/TcutxHq0lPz4iVlS9Q4xHc/RgOYl47iqdHew9JwGZzfXs63LVAJYI62kIcczpKZ6PGbA0iHaXQ==</D></RSAKeyValue>";
+        // The xml was produced with an external online tool https://the-x.cn/en-US/certificate/PemToXml.aspx
+        private static String myX509CertificatePEMinXML = @"<RSAKeyValue>
+  <D>q+RiG6tutURSV6FFHnNGLMofqh6xZeRGDQdJM9LNYi7Vt1ZGa/2rWE1GQ5bD886DAyzosYEZXT8Tfwd6scV3DKy//bJNNkLXuwNbxOQBtrRK7hMzLud4zhf5ZJWo6y6EpZGIkzqyDdKp2kjumsshIZV8ypCu3uC4P6XgXTmY92z448B263NVlTqZQz4XzLfwAYxzUXBoA6CKwV8KAww5JZBP98z3dHfwyU+K30KvSNbJRVxP31ETd40Rxwhk02t53hbtB30G6j9bdV1zM91tyQmcRrBRGOIBKCN6g2aV247x0ALrOwQTPZ+Do5pLpUEbZ0PcBB1axaYjyJrT0DwgIQ==</D>
+  <DP>rJ1jHs5pu+WzSOPIJ3vEt9bFT8tmrnx1a2O64FCPDUzg987r6yni7CENUUOrPcyL8kQslxDYCCMK/uxprZmt3rObDuCBq7u4nvdpbpIxQ9+LT3iDev8n/aJIP6fBzlyJcqzh5m2iz/XvQEo5ybCcfGEU9sUmy3eHh0etZgjNYrU=</DP>
+  <DQ>OcTna69WbLjM7p8vY2aALB/6tokizq/0OQFBI0vPkWE02P7DlVrWngoxNgZEsY9B51iMouBHWPo8eBeqHJrlksvO9/htZYLDMevWsS8UQrYhx+kqD1+PQv749sWvsyi6DwNt+QyPnxMC+/DRTkOQxqiJxO4hgSdcxpU/5JDTrFE=</DQ>
+  <Exponent>AQAB</Exponent>
+  <InverseQ>1QdnPEtQbR1QuNNPF8jCcA9cU5/SgQgChX3zY0ny7yzEq/erS8Y/aLW/BeDWrgkOHNbetwsGA4ngTxq3Xdk7Px2EZX9YHUd7uZQqCdsfr7Q4LORZciPu6dIS+NzcDRHxCPCMCRqEWLU/RzCQwIIufBNdzcVsojSONUdi/N5iT78=</InverseQ>
+  <Modulus>9e31tLqyczMHJctCI+o65MpbYERKcb0adVu3hW3uIglD9Bt2G1Q+096pQ12uI/Lqee7dSoYQ1cH7LAcBs0p2XmQ1X87hdgnbvWw3u0fP6G1SzFKEoXuJK96qlJMxBdv/Mhp3/i9pBP6j4EtXFi4PtUhyrgF45atk1p3bhfoTu9Y2xiLW7iPRKlrMpvf/goVM5uiknhf81c0Lrp2D6U1wziAGaA74dtiA61hHgW4FYYTfmeI5/E/3TnMsRhmPsJ+mwtPO51i0Sb8TcmAalLdXCN32CFztwID4JLRTh4KjcOauJl1SUcp7t9Uje09BgScQa4VYQ2RASZJ4JYmts6bLFw==</Modulus>
+  <P>+3K/Ahdg005qwxNun7AiV/Gwq80ZPao8yvCvAbFEdo9EqUr15Q/L3e1eBqEC7t8WMju9d2YiFAYpxuPny042vc8kmhBp3GkK0LE3ZjRTYkYHnoPqH8/Dti1w3plNEW/D0QM5UZUxBik3CVQxI8Jal8bVPkvkjeFQHzkZTuOxoec=</P>
+  <Q>+mGjmwIEO8izUFXV9J8AD7a09SUvLmJpwvmeqUh+DDyUaaMhm7aXM1zZUYJ1Y/ACR2KMv0IDpPFhRNtiN0+Ou4Hd5ieJuazRRH/vJ6nQ/54EVbdV5T53Q9fF/kASTH9Vvx1983sTg6RovuaB5/CtzuQflSKydwiDv8SEBIyox1E=</Q>
+</RSAKeyValue>";
         private static String myX509PrivateKeyInPEMFormat = @"-----BEGIN PRIVATE KEY-----
-                                                            MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCuFnRBS9VmiXdQ
-                                                            ndrzo80CczBY06+QSFB11rsch4YWEiQSIzub+y4lxJB1qPyOkjjIHpRs+iBSBjN0
-                                                            WVnFgOZ4Fkp250mhw7rzBMk+IAmshPIcMDBwP8rP2G4uss+xf7Wss9zV1Y/HFRuk
-                                                            atziE+kuVvB/oVgkc/XijnXw2SAnL6/StJ4BroCTjWrAvIDIrQnOL/Bysd7jS0Et
-                                                            B7qTjRB+1uxPGSASRRDv4bvXx/sOlD3NvfE5+6Tcn020IKWIKT4A9Q4C12445Hyv
-                                                            F5M6Fzh5VKtdSi7622Eo5MGs6qsyJumUUhkoDC+BDxN9qxJGVXNl/6Rj+1/G9yaV
-                                                            X+P3wPVdAgMBAAECggEAA9lZPqxXjonRioXC9ooTmU9ErWIYyp7esIxL3MieTVSf
-                                                            WYtP4RX/ojQjYYaX9usCuGH8tCAfoNCkSJlFI9nCvrr3cIOSnDIAqyja7jW1IQxb
-                                                            cgYavBCdAlVtsjif4PPK54OeiVfMcwFjeBIoyhLYn/6qGJhPo5awxA6USvvx+wvI
-                                                            gqmF9Os324gG9Q5QfznvYbdPa3CCeWFLkReyhG5YVpCHRZeiw6HE+XsVPQ54Q05d
-                                                            T3LXAM4+IfB/99tOxNvozZ1SiTkLOYz/TcutxHq0lPz4iVlS9Q4xHc/RgOYl47iq
-                                                            dHew9JwGZzfXs63LVAJYI62kIcczpKZ6PGbA0iHaXQKBgQDihSsOfDXWD9rsXoua
-                                                            j0y5KnIm/7094Md4iUWy/Jr2Kh8O+2utagVW1BieGze/+SrAREj8Bay3kz3GPXMd
-                                                            Zd4FjcKVKtsTMVs3SI8DvwQxVsDBH3YvAKSM4t/5gl0BaZkbM8s0EwjfR5Hk8Khh
-                                                            Kp/PyGq5IO3KCs8Fi2yGgiOGMwKBgQDEvm0BwEXS1xuHM9rC+HXwPMuLIKNf1jXh
-                                                            TjOF43g9Js6B9x3tC3H4yI/sa4WS7hRmx3FYNEPOsJ+O0iVh8Wl9ykZo2MRFXsYS
-                                                            jhyDcGPu/CRXXomQ9ZglXbRW9+jdN+/ULwhSY9ATZ+kkvdyHcrDXuvLEJJsz4kwD
-                                                            jkfz1DFmLwKBgQCJ3wXq5h0ackIMlisgYtIO7c/W/tdfHz4plluu6mMjK1YGF29d
-                                                            eGdRhvyI1wYiuXSU27sUchvDDEKQM3yqSwNMnBDwG8qFrwfW6TnqzMuvgB8ILK8R
-                                                            QrHk8n3ibr9PCcffr7Ghu2guBnC9SOEjav8kR9b1YR56WAPTWx4t7G3U0wKBgQCy
-                                                            wXXXRnPqWgkn8aRMy2SUzxf8o73swHsZ52yGtKM1pIGyse0dx+1YphmmLBcj5W7h
-                                                            50oDYxFMxUtjf2n5sFcxtEv+zRyfDroWd8pOXhpeM/ds7nc9HveK07Wb4wlPnZgp
-                                                            CEaeXRPb3eiFun+ZDUrB55jVP5FINc7s2CRl1KDBfwKBgQC6Oq+04cDMFIm4n6ef
-                                                            078klSU/sV9H1TU/PEcGHbHwC+ZkdDXqf8TxkTGAaadBSLit0Av0Q26soDYMJhbt
-                                                            xd8g/F0sj6NS6UURScPojXzKKntNbe8gqcO0vXRgPQBSinm1VZ6JNBVvJcaSm46W
-                                                            BMdrhBsPNRoE4X/sk+r2Mdq9Bg==
-                                                            -----END PRIVATE KEY-----";
+MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQD17fW0urJzMwcl
+y0Ij6jrkyltgREpxvRp1W7eFbe4iCUP0G3YbVD7T3qlDXa4j8up57t1KhhDVwfss
+BwGzSnZeZDVfzuF2Cdu9bDe7R8/obVLMUoShe4kr3qqUkzEF2/8yGnf+L2kE/qPg
+S1cWLg+1SHKuAXjlq2TWnduF+hO71jbGItbuI9EqWsym9/+ChUzm6KSeF/zVzQuu
+nYPpTXDOIAZoDvh22IDrWEeBbgVhhN+Z4jn8T/dOcyxGGY+wn6bC087nWLRJvxNy
+YBqUt1cI3fYIXO3AgPgktFOHgqNw5q4mXVJRynu31SN7T0GBJxBrhVhDZEBJkngl
+ia2zpssXAgMBAAECggEBAKvkYhurbrVEUlehRR5zRizKH6oesWXkRg0HSTPSzWIu
+1bdWRmv9q1hNRkOWw/POgwMs6LGBGV0/E38HerHFdwysv/2yTTZC17sDW8TkAba0
+Su4TMy7neM4X+WSVqOsuhKWRiJM6sg3SqdpI7prLISGVfMqQrt7guD+l4F05mPds
++OPAdutzVZU6mUM+F8y38AGMc1FwaAOgisFfCgMMOSWQT/fM93R38MlPit9Cr0jW
+yUVcT99RE3eNEccIZNNred4W7Qd9Buo/W3VdczPdbckJnEawURjiASgjeoNmlduO
+8dAC6zsEEz2fg6OaS6VBG2dD3AQdWsWmI8ia09A8ICECgYEA+3K/Ahdg005qwxNu
+n7AiV/Gwq80ZPao8yvCvAbFEdo9EqUr15Q/L3e1eBqEC7t8WMju9d2YiFAYpxuPn
+y042vc8kmhBp3GkK0LE3ZjRTYkYHnoPqH8/Dti1w3plNEW/D0QM5UZUxBik3CVQx
+I8Jal8bVPkvkjeFQHzkZTuOxoecCgYEA+mGjmwIEO8izUFXV9J8AD7a09SUvLmJp
+wvmeqUh+DDyUaaMhm7aXM1zZUYJ1Y/ACR2KMv0IDpPFhRNtiN0+Ou4Hd5ieJuazR
+RH/vJ6nQ/54EVbdV5T53Q9fF/kASTH9Vvx1983sTg6RovuaB5/CtzuQflSKydwiD
+v8SEBIyox1ECgYEArJ1jHs5pu+WzSOPIJ3vEt9bFT8tmrnx1a2O64FCPDUzg987r
+6yni7CENUUOrPcyL8kQslxDYCCMK/uxprZmt3rObDuCBq7u4nvdpbpIxQ9+LT3iD
+ev8n/aJIP6fBzlyJcqzh5m2iz/XvQEo5ybCcfGEU9sUmy3eHh0etZgjNYrUCgYA5
+xOdrr1ZsuMzuny9jZoAsH/q2iSLOr/Q5AUEjS8+RYTTY/sOVWtaeCjE2BkSxj0Hn
+WIyi4EdY+jx4F6ocmuWSy873+G1lgsMx69axLxRCtiHH6SoPX49C/vj2xa+zKLoP
+A235DI+fEwL78NFOQ5DGqInE7iGBJ1zGlT/kkNOsUQKBgQDVB2c8S1BtHVC4008X
+yMJwD1xTn9KBCAKFffNjSfLvLMSr96tLxj9otb8F4NauCQ4c1t63CwYDieBPGrdd
+2Ts/HYRlf1gdR3u5lCoJ2x+vtDgs5FlyI+7p0hL43NwNEfEI8IwJGoRYtT9HMJDA
+gi58E13NxWyiNI41R2L83mJPvw==
+-----END PRIVATE KEY-----";
 
         // This is the .cer file content without th ----BEGIN CERTIFICATE---- ----END CERTIFICATE---- text
-        private static String myX509CertificateCER = @"MIIDsDCCApgCCQD+QLYZSAiEwDANBgkqhkiG9w0BAQsFADCBmTELMAkGA1UEBhMC
-                                                    R1IxDzANBgNVBAgMBkF0dGljYTEPMA0GA1UEBwwGQXRoZW5zMQwwCgYDVQQKDANF
-                                                    UEExDzANBgNVBAsMBkVQQSBJVDEeMBwGA1UEAwwVZnlzaWtvYWVyaW9lbGxhZG9z
-                                                    LmdyMSkwJwYJKoZIhvcNAQkBFhppbmZvQGZ5c2lrb2FlcmlvZWxsYWRvcy5ncjAe
-                                                    Fw0yMjA0MTAxMDE3MzZaFw0yNjAzMjAxMDE3MzZaMIGZMQswCQYDVQQGEwJHUjEP
-                                                    MA0GA1UECAwGQXR0aWNhMQ8wDQYDVQQHDAZBdGhlbnMxDDAKBgNVBAoMA0VQQTEP
-                                                    MA0GA1UECwwGRVBBIElUMR4wHAYDVQQDDBVmeXNpa29hZXJpb2VsbGFkb3MuZ3Ix
-                                                    KTAnBgkqhkiG9w0BCQEWGmluZm9AZnlzaWtvYWVyaW9lbGxhZG9zLmdyMIIBIjAN
-                                                    BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArhZ0QUvVZol3UJ3a86PNAnMwWNOv
-                                                    kEhQdda7HIeGFhIkEiM7m/suJcSQdaj8jpI4yB6UbPogUgYzdFlZxYDmeBZKdudJ
-                                                    ocO68wTJPiAJrITyHDAwcD/Kz9huLrLPsX+1rLPc1dWPxxUbpGrc4hPpLlbwf6FY
-                                                    JHP14o518NkgJy+v0rSeAa6Ak41qwLyAyK0Jzi/wcrHe40tBLQe6k40QftbsTxkg
-                                                    EkUQ7+G718f7DpQ9zb3xOfuk3J9NtCCliCk+APUOAtduOOR8rxeTOhc4eVSrXUou
-                                                    +tthKOTBrOqrMibplFIZKAwvgQ8TfasSRlVzZf+kY/tfxvcmlV/j98D1XQIDAQAB
-                                                    MA0GCSqGSIb3DQEBCwUAA4IBAQBmnsGGLqc7IsbMYlxU2/w7UKeEDor59Lb60DoW
-                                                    snZ2j7t3YwTsAWIquCOyduGQVLiPR5/wy+LcSVuAf34NA0z/K+oQmqgqAhFAtZqy
-                                                    bP52yzNiA2rAdh5E70yYSp9mSekvcxN5ozbWIYwJzb7Wih2cPWj5llWSBpIGp3CM
-                                                    g0gLNGeXNO2MbeLIIs0td2bm5ufPKqBUzYgXv5XqPcGMYEARibzXoArmo3UpzeVw
-                                                    WJgGwOYYO++7WhOFCuH0ZneFUtGVAAjzSoTEJz5AKXhZUq8FjLJ9zMILEiGelvBa
-                                                    yihzicf67VSwAMgaN3/oPOiS57iF7tZ0K67nWdppF/cnwfnP";
+        private static String myX509CertificateCER = @"MIIESzCCAzOgAwIBAgIJANq6Drpt8SB5MA0GCSqGSIb3DQEBCwUAMHYxCzAJBgNV
+BAYTAkVFMREwDwYDVQQIEwhIYXJqdW1hYTEQMA4GA1UEBxMHVGFsbGlubjEQMA4G
+A1UEChMHTW9kaXJ1bTEUMBIGA1UECxMLRGV2ZWxvcG1lbnQxGjAYBgNVBAMTEWlu
+dGVyZmFjZTRTaWdua2V5MB4XDTE3MTExNjE0MDUwN1oXDTMxMDcyNjE0MDUwN1ow
+djELMAkGA1UEBhMCRUUxETAPBgNVBAgTCEhhcmp1bWFhMRAwDgYDVQQHEwdUYWxs
+aW5uMRAwDgYDVQQKEwdNb2RpcnVtMRQwEgYDVQQLEwtEZXZlbG9wbWVudDEaMBgG
+A1UEAxMRaW50ZXJmYWNlNFNpZ25rZXkwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAw
+ggEKAoIBAQD17fW0urJzMwcly0Ij6jrkyltgREpxvRp1W7eFbe4iCUP0G3YbVD7T
+3qlDXa4j8up57t1KhhDVwfssBwGzSnZeZDVfzuF2Cdu9bDe7R8/obVLMUoShe4kr
+3qqUkzEF2/8yGnf+L2kE/qPgS1cWLg+1SHKuAXjlq2TWnduF+hO71jbGItbuI9Eq
+Wsym9/+ChUzm6KSeF/zVzQuunYPpTXDOIAZoDvh22IDrWEeBbgVhhN+Z4jn8T/dO
+cyxGGY+wn6bC087nWLRJvxNyYBqUt1cI3fYIXO3AgPgktFOHgqNw5q4mXVJRynu3
+1SN7T0GBJxBrhVhDZEBJknglia2zpssXAgMBAAGjgdswgdgwHQYDVR0OBBYEFIma
+dY2XUUEADcnAi2qwi433Npp+MIGoBgNVHSMEgaAwgZ2AFImadY2XUUEADcnAi2qw
+i433Npp+oXqkeDB2MQswCQYDVQQGEwJFRTERMA8GA1UECBMISGFyanVtYWExEDAO
+BgNVBAcTB1RhbGxpbm4xEDAOBgNVBAoTB01vZGlydW0xFDASBgNVBAsTC0RldmVs
+b3BtZW50MRowGAYDVQQDExFpbnRlcmZhY2U0U2lnbmtleYIJANq6Drpt8SB5MAwG
+A1UdEwQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAL0bSHmsVF/Pys1vLmehFIvk
+jopF2plRVklCalMiy+IJx8N9A91PsCbc2veykvIlFtEwVoVZGPtSlNDUl9HsSH/A
+zcatBRWe/Iz1W/4rMa1UWZsk2DDw6bjvo1KPYIvHBufUB4IXm/qyFL7IohYcWF/s
+w0y+XMrvcd3c7ClZ1mq43GKnlHkXwaPWHoMnuviguIbgKAHKVT9pgqCZQSoIjN08
+oejCa7qVlYuUY5EGwzuHNErntmcgicP7sLWd4Pu1fAx+51tgDSGjh2m0SSDz2rv7
+CrJ44RXIUOWAMWbC4myssyea3t+GrSvWrDGHRLXZUNmvy+zFSB+QFWEW2nlfYI8=";
 
         public static string sign(string valueToSign)
         {
             //byte[] keyBytes = Encoding.Unicode.GetBytes(myX509Certificate);
-            byte[] toSign = System.Text.Encoding.Unicode.GetBytes(valueToSign);
+            byte[] toSign = System.Text.Encoding.UTF8.GetBytes(valueToSign);
 
             RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
             RSAalg.ImportFromPem(myX509PrivateKeyInPEMFormat.ToCharArray());
-            byte[] signature = RSAalg.SignData(toSign, "SHA256");
+            byte[] signature = RSAalg.SignData(toSign, SHA256.Create());
             string sgStr = Convert.ToBase64String(signature);
 
             return sgStr;
@@ -84,7 +96,7 @@ namespace RSACryptography
 
             RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
             provider.FromXmlString(myX509CertificatePEMinXML);
-            byte[] signedBytes = provider.SignData(Encoding.Unicode.GetBytes(valueToSign), "SHA256");
+            byte[] signedBytes = provider.SignData(Encoding.UTF8.GetBytes(valueToSign), SHA256.Create());
 
             return Convert.ToBase64String(signedBytes);
 
@@ -96,7 +108,7 @@ namespace RSACryptography
 
             RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
             provider.FromXmlString(PEM2XML(myX509PrivateKeyInPEMFormat));
-            byte[] signedBytes = provider.SignData(Encoding.Unicode.GetBytes(valueToSign), "SHA256");
+            byte[] signedBytes = provider.SignData(Encoding.UTF8.GetBytes(valueToSign), SHA256.Create());
 
             return Convert.ToBase64String(signedBytes);
 
